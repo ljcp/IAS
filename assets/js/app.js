813a4b825d21a5105d89cb1be53fc2cde3b9505f
Lance <lancebeaudry@gmail.com>
2016-01-11 23:14:16 -0500

    Form Changes
    Added jQuery validate framework.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,2 +1,27 @@
 var app = angular.module("MyApp", ['ngAnimate', 'ui.bootstrap']);
 
+$(document).ready(function () {
+    'use strict';
+    //Form
+    $('#submit').click(function (e) {
+        
+        //Stop form submission & check the validation
+        e.preventDefault();
+        
+        $.post("assets/php/form.php", $("#quote-form").serialize(), function (result) {
+ //Check the result set from contact.php file.
+            if (result == 'sent') {
+ //If the email is sent successfully, remove the submit button
+                $('#submit-area').remove();
+ //Display the success message
+                $('#success').fadeIn(500);
+            } else {
+ //Display the error message
+                $('#fail').fadeIn(500);
+ // Enable the submit button again
+                $('#submit').removeAttr('disabled').attr('value', 'Retry');
+            }
+        });
+    });
+});
+
--- assets/php/form.php
+++ assets/php/form.php
@@ -3,15 +3,22 @@ $name  = $_REQUEST["name"];
 $email = $_REQUEST["email"];
 $phone   = $_REQUEST["phone"];
 $cname   = $_REQUEST["cname"];
-$cname   = $_REQUEST["cname"];
+$ventilation = $_REQUEST["ventilation"];
+$fans = $_REQUEST["fans"];
+$dust = $_REQUEST["dust"];
+$conveying = $_REQUEST["conveying"];
+$pollution = $_REQUEST["pollution"];
+$bags = $_REQUEST["bags"];
+$nfpa = $_REQUEST["nfpa"];
+$other = $_REQUEST["other"];
 $msg   = $_REQUEST["msg"];
 $to    = "lancebeaudry@gmail.com"; /* <= Change this Email ID to yours */
 if (isset($email) && isset($name) && isset($msg)) {
-    $subject = "$name sent you a message via Business Name"; /* <= Change the Subject If you want */
+    $subject = "$name sent you a message via ias-mi.com"; /* <= Change the Subject If you want */
 		$headers = "MIME-Version: 1.0" . "\r\n";
 $headers .= "Content-type:text/html;charset=iso-8859-1" . "\r\n";
 $headers .= "From: ".$name." <".$email.">\r\n"."Reply-To: ".$email."\r\n" ;
-$msg     = "From: $name<br/> Email: $email <br/>Message: $msg";
+$msg     = "From: $name<br/> Email: $email <br/>Message: $msg <br/>Industrial Ventilation: $ventilation <br/>Fans & Makeup Air: $fans <br/>Dust/Mist Collection: $dust <br/>Pneumatic Conveying: $conveying <br/>Air & Noise Pollution: $pollution <br/>Replacement Bags: $bags <br/>NFPA Compliance: $nfpa <br/>Other: $other";
 	
    $mail =  mail($to, $subject, $msg, $headers);
   if($mail)
